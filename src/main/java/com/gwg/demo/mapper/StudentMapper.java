4876dccd73e46535c6bb37808a9cd90c59b9b3b1
admin <weigang.gao>
2019-03-18 20:57:43 +0800

    基于starter的druid和mybatis配额制

---
+++ src/main/java/com/gwg/demo/mapper/StudentMapper.java
@@ -0,0 +1,9 @@
+package com.gwg.demo.mapper;
+
+import com.gwg.demo.model.Student;
+
+public interface StudentMapper {
+	
+	public Student queryStudentByName(String name);
+
+}
---
+++ src/main/java/com/gwg/demo/model/Student.java
@@ -0,0 +1,30 @@
+package com.gwg.demo.model;
+
+/**
+ * 实体类
+ * @author gaoweigang
+ *
+ */
+public class Student {
+	
+	private String name;
+	
+	private int age;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+}
